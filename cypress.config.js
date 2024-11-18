217992a2c5bd054e84497d80c0ed6071d0d721e7
Tiago França <dev@tiagofranca.com>
2024-11-18 08:05:28 -0300

    style and similar changes
    	modified:   .editorconfig
    	modified:   .prettierrc.json
    	modified:   .vscode/extensions.json
    	modified:   cypress.config.js
    	modified:   cypress/e2e/example.cy.js
    	new file:   cypress/support/component-index.html
    	new file:   cypress/support/component.js
    	modified:   index.html
    	modified:   jsconfig.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.vue
    	new file:   src/components/HelloWorld.cy.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,4 +5,11 @@ export default defineConfig({
         specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
         baseUrl: 'http://localhost:4173',
     },
+
+    component: {
+        devServer: {
+            framework: 'vue',
+            bundler: 'vite',
+        },
+    },
 });
--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -3,6 +3,6 @@
 describe('My First Test', () => {
     it('visits the app root url', () => {
         cy.visit('/');
-        cy.contains('h1', 'You did it!');
+        cy.contains('a.router-link-active', 'Home'); // old: You did it!
     });
 });
